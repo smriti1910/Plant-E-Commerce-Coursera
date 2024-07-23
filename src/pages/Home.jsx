c11f876763100c27700184bb4e4d224eee42a87b
smriti1910 <smritibalaji19@gmail.com>
2024-07-23 10:53:40 +0530

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
     return (
         <div className="home">
             <div className="home-content">
-                <h1>Welcome to Siddarthan Plant Store</h1>
+                <h1>Welcome to Plant Store</h1>
                 <p>Discover a wide variety of houseplants to brighten your home or office. Our plants are carefully selected to ensure the highest quality and beauty.</p>
                 <button onClick={() => window.location.href = '/products'}>Get Started</button>
             </div>
